aa332d52717bc5a30f120f9287f79ff7d9c8aa26
Mathieu Tourret <pablolola@live.fr>
2021-01-24 00:48:58 +0100

    api server start

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express')
+const cors = require('cors') 
 const app = express()
 
 const port = 3000
 
+app.use(cors)
 app.use(express.json)
 
 app.post('/api',async (req,res) => {
