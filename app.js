0bda428949aa1b1e2e8a6662eaca5cd7ab73f0d6
Mathieu Tourret <pablolola@live.fr>
2021-01-24 00:56:52 +0100

    api server start

--- app.js
+++ app.js
@@ -7,6 +7,15 @@ const port = 3000
 /*app.use(cors)
 app.use(express.json)*/
 
+app.get('/api',async (req,res) => {
+    try {
+        console.log(req.body)
+        res.send(req.body)
+    }catch(err) {
+        console.log(err.message)
+    }
+})
+
 app.post('/api',async (req,res) => {
     try {
         console.log(req.body)
