195f04ccb39d4507812e2900dc4f5da2e1815d4e
Mathieu Tourret <pablolola@live.fr>
2021-01-24 19:03:21 +0100

    install pg

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const cors = require('cors') 
 const app = express()
 const port = 3000
-const pool = require('db')
+const pool = require('./db')
 
 app.use(cors())
 app.use(express.json())
