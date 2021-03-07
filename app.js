c04f07d71edda3b5bcf638658afea0756b045884
Mathieu Tourret <pablolola@live.fr>
2021-03-07 14:58:37 +0100

    ordering tables

--- app.js
+++ app.js
@@ -117,7 +117,7 @@ app.route('/user/update/:id').patch(async (req, res) => {
     
     const settingString = dataTab.join();
     console.log(settingString);
-    const updatedUser = await pool.query(`UPDATE Users SET ${settingString} WHERE userid = $1 RETURNING *`,[id]);
+    const updatedUser = await pool.query(`UPDATE users SET ${settingString} WHERE id = $1 RETURNING *`,[id]);
     res.json(updatedUser.rows);
   }catch(err){
     res.json({
