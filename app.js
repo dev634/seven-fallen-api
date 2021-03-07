c9cd10d8f19037a3e1c3d440b025f923e4817db4
Mathieu Tourret <pablolola@live.fr>
2021-03-07 13:46:22 +0100

    change route users

--- app.js
+++ app.js
@@ -76,9 +76,9 @@ app.route("/user").post(async (req, res) => {
     const {username,email,password} = req.body;
     const hashedPassword = await bcrypt.hash(password,saltRounds); 
     const newUser = await pool.query("INSERT INTO Users(username,email,password) VALUES($1,$2,$3) RETURNING *",[username,email,hashedPassword]);
-    res.json(newUser)
+    res.json(newUser.rowsCount)
   } catch (err) {
-        res.json({
+        res.status(404).json({
             message: err.message,
         })
   }
