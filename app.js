50d42f68e25bd4a152685a2d25de3d6798648ee5
Mathieu Tourret <pablolola@live.fr>
2021-03-07 13:58:51 +0100

    ordering tables

--- app.js
+++ app.js
@@ -75,7 +75,9 @@ app.route("/user").post(async (req, res) => {
   try {
     const {username,email,password} = req.body;
     const hashedPassword = await bcrypt.hash(password,saltRounds); 
-    const newUser = await pool.query("INSERT INTO Users(username,email,password) VALUES($1,$2,$3) RETURNING username,email",[username,email,hashedPassword]);
+    const newUser = await pool.query("INSERT INTO Users(username,email,password) VALUES($1,$2,$3) RETURNING username,email",[username,email,hashedPassword]).catch(() => {
+      throw new Error('Bad request or this user already exist ...')
+    });
 
     res.json(newUser)
   } catch (err) {
@@ -83,7 +85,6 @@ app.route("/user").post(async (req, res) => {
             status : err.statusCode,
             message: err.message
         })
-        res.json(newUser)
   }
 });
 
