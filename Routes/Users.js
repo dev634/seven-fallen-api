28173269db0171e9817cef029fdb9043de8b6df8
Mathieu Tourret <pablolola@live.fr>
2021-03-12 10:57:57 +0100

    refactorisation users

--- Routes/Users.js
+++ Routes/Users.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const getAllUsers = require('../Models/Users');
+const { getAllUsers } = require('../Models/Users');
 
 
 //Get all users
