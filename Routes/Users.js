df02465e5c052dbff6cabe2a38568a718c45917c
Mathieu Tourret <pablolola@live.fr>
2021-03-12 11:21:44 +0100

    refactorisation users

--- Routes/Users.js
+++ Routes/Users.js
@@ -9,20 +9,20 @@ const Users = require('../Models/Users');
 router.get('/users', async (req,res) => {
     try{
         const users = new Users();
+        // if(getUsers.rowCount === 0){
+        //   res.status(404).json({
+        //     status : res.statusCode,
+        //     message : "'Resources not found...'"
+        //   });
+        // }
+        // res.json(getUsers.rows);
         console.log(users);
-    //     if(getUsers.rowCount === 0){
-    //       res.status(404).json({
-    //         status : res.statusCode,
-    //         message : "'Resources not found...'"
-    //       });
-    //     }
-    //     res.json(getUsers.rows);
-    // }catch(err){
-    //     res.status(404).json({
-    //         status : res.statusCode,
-    //         message: err.message,
-    //     });
-    // }
+    }catch(err){
+        res.status(404).json({
+            status : res.statusCode,
+            message: err.message,
+        });
+    }
 });
 
 module.exports = router;
