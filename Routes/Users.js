7fcf1abcc76a57b653e3aeb6304e8a7092974f21
Mathieu Tourret <pablolola@live.fr>
2021-03-12 11:24:29 +0100

    refactorisation users

--- Routes/Users.js
+++ Routes/Users.js
@@ -9,6 +9,7 @@ const Users = require('../Models/Users');
 router.get('/users', async (req,res) => {
     try{
         const users = new Users();
+        users.getAllUsers();
         // if(getUsers.rowCount === 0){
         //   res.status(404).json({
         //     status : res.statusCode,
@@ -16,7 +17,7 @@ router.get('/users', async (req,res) => {
         //   });
         // }
         // res.json(getUsers.rows);
-        console.log(users);
+        console.log(users.data);
     }catch(err){
         res.status(404).json({
             status : res.statusCode,
