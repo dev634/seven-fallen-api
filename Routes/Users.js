6724c0be0a750e41394af537498542be0ac18fc1
Mathieu Tourret <pablolola@live.fr>
2021-03-12 11:11:28 +0100

    refactorisation users

--- Routes/Users.js
+++ Routes/Users.js
@@ -7,7 +7,7 @@ const getAllUsers = require('../Models/Users');
 
 router.get('/users', async (req,res) => {
     try{
-        const getUsers = await getAllUsers();
+        const getUsers = getAllUsers;
         if(getUsers.rowCount === 0){
           res.status(404).json({
             status : res.statusCode,
