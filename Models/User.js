dd3a4754ad68c66fec4acfd04c528b0942c9ae11
Mathieu Tourret <pablolola@live.fr>
2021-03-13 12:38:37 +0100

    refacto du getUser

--- Models/User.js
+++ Models/User.js
@@ -1,24 +1,25 @@
 const pool = require('../db');
 
 const getUser = (req, res) => {
-
-      const id = req.params.id;
-      pool.query('SELECT username, email FROM users WHERE id = $1',[id], (err,result) => {
-        try{
-            if(err){
-                throw new Error('User not found...');
+        const id = req.params.id;
+        pool.query('SELECT username, email FROM users WHERE id = $1',[id], (err,result) => {
+          try{
+              if(err){
+                  err.code = 422;
+                  err.message = "unable to procees this request"
+                  throw err;
+              }
+  
+              if(result.rowCount === 0){
+                  throw new Error('User not found...');
+              }
+          }catch(err){
+              res.status(404).json({
+                code : res.statusCode,
+                message: err.message
+              });
             }
-
-            if(result.rowCount === 0){
-                throw new Error('User not found...');
-            }
-        }catch(err){
-            res.status(404).json({
-              code : res.statusCode,
-              message: err.message
-            });
-          }
-      });
+        });
 }
 
 module.exports = {
