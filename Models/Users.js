693a273cdb7d6d9da098b7e6f33b88a03b5c9b1e
Mathieu Tourret <pablolola@live.fr>
2021-03-13 11:33:28 +0100

    refacto users route

--- Models/Users.js
+++ Models/Users.js
@@ -4,7 +4,8 @@ const pool = require('../db');
 const getAllUsers = (req,res) => {
     pool.query('SELECT username,email FROM users', (err,results) => {
         if(err){
-            throw err
+            err.message = "Not found";
+            res.status(404).json(err.message);
         }
         res.status(200).json(results.rows);
     });
