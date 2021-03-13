771ce17bce7cc43cc60db9b3869ea25bdcd87656
Mathieu Tourret <pablolola@live.fr>
2021-03-13 12:46:37 +0100

    refacto du getUser

--- Models/User.js
+++ Models/User.js
@@ -4,18 +4,15 @@ const getUser = (req, res) => {
         const id = req.params.id;
         pool.query('SELECT username, email FROM users WHERE id = $1',[id], (err,result) => {
           try{
-              if(err && result.rowCount === 0){
-                console.log(err)
+              if(err){
+                  err.code = 422;
+                  err.message = "unable to process this request"
+                  throw err;
               }
-            //   if(err){
-            //       err.code = 422;
-            //       err.message = "unable to process this request"
-            //       throw err;
-            //   }
   
-            //   if(result.rowCount === 0){
-            //       throw {code: 404, message: 'not found ..'}
-            //   }
+              if(result.rowCount === 0){
+                  throw {code: 404, message: 'not found ..'}
+              }
           }catch(err){
               res.status(err.code).json({
                 code : res.statusCode,
