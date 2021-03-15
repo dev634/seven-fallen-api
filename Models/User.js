376d7009ea51a0c2d6e34f9e34977b5c67235904
Mathieu Tourret <pablolola@live.fr>
2021-03-15 21:41:02 +0100

    bug form fix

--- Models/User.js
+++ Models/User.js
@@ -103,7 +103,7 @@ const deleteUser = async (req,res) => {
         console.log(regex.test(id));
         let exist = null;
         
-        if(regex.test(id) === true){
+        if(!regex.test(id)){
             exist = await pool.query('SELECT username,email FROM users WHERE id = $1',[id]);
             console.log(exist.rows)
         }else{
