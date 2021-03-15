84881b2040773027aea7772a0ca9f281aba6f347
Mathieu Tourret <pablolola@live.fr>
2021-03-15 22:43:02 +0100

    bug form fix

--- Models/User.js
+++ Models/User.js
@@ -98,7 +98,7 @@ const updateUser = (req, res) => {
 
 const deleteUser = async (req,res) => {
     try {
-        const regex = /^[^0-9]+$/g;
+        const regex = /^[0-9]+$/g;
         const id = req.params.id;
         let exist = null;
         
